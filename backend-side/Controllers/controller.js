5676443ca77252efc4fb364dcba6bcb48fd7912b
Arslan Ahmed <arslanahmed.kcsf18@iba-suk.edu.pk>
2022-07-19 15:53:56 +0500

    delete bookmark functionality added

--- backend-side/Controllers/controller.js
+++ backend-side/Controllers/controller.js
@@ -9,6 +9,26 @@ const add = async(req,res)=>{
 }
 
 
+const getbookmark = (req,res)=>{
+    const bookmarks=await Bookmark.find()
+    res.json({bookmarks:bookmarks})
+}
+
+const getwithname = async(req,res)=>{
+
+    const name=req.params.name
+    const bookmarks=await Bookmark.find({name:name})
+    res.json({bookmarks:bookmarks})
+
+}
+
+const deletebookmark = (req,res)=>{
+    const id = req.params.id
+    await Bookmark.remove({_id:id})
+    res.json({status:"success"})
+}
+
+
 module.exports = {
     add
 }
